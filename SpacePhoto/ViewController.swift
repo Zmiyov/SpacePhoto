c7bb28a9a5f8e4195fafaf7043db37bcb202c4b8
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-06-21 15:12:10 +0300

    Initial Commit

---
+++ SpacePhoto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpacePhoto
+//
+//  Created by Vladimir Pisarenko on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
